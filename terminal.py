2cef9370d23014412e5b53acd255014f2367130a
wi6n3l <35975131+wi6n3l@users.noreply.github.com>
2019-07-21 19:33:27 +0100

    Banner changes
    Banner changes (added color effects to linux users)

--- terminal.py
+++ terminal.py
@@ -1,40 +1,56 @@
 import socket, time, os, platform
 
+global author_instagram
+author_instagram = "tchewui"
+
 class symbols():
-    sucess = "\033[32m[+] "
-    error = "\033[31m[-] "
-    warning = "\033[33m[#] "
-    info = "\033[34m[!] "
-    #if (platform.system() != "Linux"):##########debugging
-    if(platform.system() == "Windows"):
-        sucess = "[+] "
-        error = "[-] "
-        warning = "[#] "
-        info = "[!] "
-
+    def __init__(self):
+        self.defaults = "\033[37m"
+        self.banners = "\033[36m"
+        self.credits = "\033[34mmade by \033[31m@" + author_instagram+"\033[39m"
+        self.sucesss = "\033[32m[+] "
+        self.errors = "\033[31m[-] "
+        self.warnings = "\033[33m[#] "
+        self.infos = "\033[34m[!] "
+        #if (platform.system() != "Linux"):##########debugging
+        if(platform.system() != "Windows"):
+            self.defaults = ""
+            self.banners = ""
+            self.credits = "made by @" + author_instagram
+            self.sucesss = "[+] "
+            self.errors = "[-] "
+            self.warnings = "[#] "
+            self.infos = "[!] "
+    def default(self, text):
+        return self.defaults + str(text)
+    def banner(self, text):
+        return self.banners + str(text)
+    def credit(self):
+        return self.credits
     def sucess(self, text):
-        return self.sucess + str(text)
+        return self.sucesss + str(text)
     def error(self, text):
-        return self.error + str(text)
+        return self.errors + str(text)
     def warning(self, text):
-        return self.warning + str(text)
+        return self.warnings + str(text)
     def info(self, text):
-        return self.info + str(text)
-#print(color.green + "+")
-#print(color.red + "-")
-#print(color.blue + "!")
+        return self.infos + str(text)
 
-print("""
-###################################################################################
-###################################################################################
-##||||||############|||||||########################################################
-##||  ||############||   ||##|||||###########||#####    ||#########################
-##||||||#\\\\  //#####|||||||###   ||##||||||##|| //##    ||##||||||##||||||##|| __##
-##||######\\\\//######||   ||##||||||##||    ##||// ##||||||##||  ||##||  ||##||//-##
-##||#######//#######||    ||#||  ||##||    ##||\\\\ ##||  ||##||  ||##||  ||##||   ##
-##||######//########|||||||##||||||##||||||##|| \\\\##||||||##||||||##||||||##||   ##
-###################################################################################
-###################################################################################""")
+symbols = symbols()
+
+print(symbols.default("###################################################################################\n" +
+                      "###################################################################################\n" +
+                      "##")+symbols.banner("||||||") + symbols.default("############") + symbols.banner("|||||") + symbols.default("##########################################################\n" +
+                      "##" + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("############") + symbols.banner("||") + symbols.default("   ") + symbols.banner("|") + symbols.default("###") + symbols.banner("|||||") + symbols.default("###########") + symbols.banner("||") + symbols.default("#########") + symbols.banner("||") + symbols.default("#########################\n" +
+                      "##") + symbols.banner("||||||") + symbols.default("#") + symbols.banner("\\\\") + symbols.default("  ") + symbols.banner("//") + symbols.default("#####") + symbols.banner("||||||") + symbols.default("####   ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||") + symbols.default(" ") + symbols.banner("//") + symbols.default("######") + symbols.banner("||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||") + symbols.default(" ") + symbols.banner("__") + symbols.default("##\n" +
+                      "##") + symbols.banner("||") + symbols.default("######") + symbols.banner("\\\\//") + symbols.default("######") + symbols.banner("||") + symbols.default("   ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||") + symbols.default("    ##") + symbols.banner("||//") + symbols.default(" ##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||//") + symbols.default("###\n" +
+                      "##") + symbols.banner("||") + symbols.default("#######") + symbols.banner("//") + symbols.default("#######") + symbols.banner("||") + symbols.default("    ") + symbols.banner("|") + symbols.default("##") + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||") + symbols.default("    ##") + symbols.banner("||\\\\") + symbols.default(" ##") + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||") + symbols.default("  ") + symbols.banner("||") + symbols.default("##") + symbols.banner("||") + symbols.default("#####\n" +
+                      "##") + symbols.banner("||") + symbols.default("######") + symbols.banner("//") + symbols.default("########") + symbols.banner("|||||||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||") + symbols.default(" ") + symbols.banner("\\\\") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||||||") + symbols.default("##") + symbols.banner("||") + symbols.default("#####\n" +
+                      "###################################################################################\n" +
+                      "###################################################################################\n\n")) +
+                      " "*(93-len(symbols.credit())) + symbols.credit())
+
+
 
 def create_backdoor():
     print(symbols.info("in progress"))
@@ -47,8 +63,7 @@ def listen_multiple_sessions():
 
 
 def changeMenu():
-    print("""
-        Menu 
+    print("""        Menu 
 1. Create Backdoor
 2. Listen
 3. Listen Multiple Sessions
